b5236a0fbf39cf7d8df8ffb3cf9f6360c4a4f874
MrPotatoXx <salinasgiovanni25@gmail.com>
2022-07-01 02:23:18 -0400

    Se crea el primer controlador de usuario (Crear)

---
+++ controllers/usuarios_controller.js
@@ -0,0 +1,29 @@
+const Usuario = require('../models/usuarios_model');
+let response = {
+    msg: "",
+    exito: false
+}
+
+exports.create = function(req, res){
+    let usuario = new Usuario({
+        nombre: req.body.nombre,
+        apellido: req.body.apellido,
+        email: req.body.email,
+        password: req.body.password,
+        telefono: req.body.telefono,
+        tipo: req.body.tipo
+    });
+
+    usuario.save(function(err){
+        if(err){
+            console.log(err);
+            response.exito = false;
+            response.msg = "Error al crear el usuario";
+            res.json(response);
+            return;
+        }
+        response.exito = true;
+        response.msg = "Usuario creado correctamente";
+        res.json(response);
+    });
+}
