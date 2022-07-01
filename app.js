6f54a55b86ac85ff9501d181265587537f66fd14
MrPotatoXx <salinasgiovanni25@gmail.com>
2022-07-01 02:36:59 -0400

    Se crea el enrutador para usuarios

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var database = require('./config/database');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var usuariosRouter = require('./routes/usuarios_routes');
 
 var app = express();
 
@@ -23,6 +24,7 @@ database.mongoConnect();
 // routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/usuarios', usuariosRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
